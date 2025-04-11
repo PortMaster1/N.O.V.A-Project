d5656e54add61dd26e735984f0e186e07e3863b3
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-11 17:01:44 -0400

    Update main.py

--- Onboard-Setup/main.py
+++ Onboard-Setup/main.py
@@ -10,11 +10,19 @@ from memory_lane import remember, forget
 def main_loop():
     user_input = server.listen()
     response = get_response(user_input)
-    for search in [r'\[REMEMBER: (.*?)\]', r'\[FORGET: (.*?)\]']:
-        match = re.search(search, response)
-        if match:
-            text = match.group(1).split()
-            action = text[0]
-            args = text[1:]
+    match = re.search(r'\[REMEMBER: (.*?)\]', response)
+    if match:
+        text = match.group(1).split()
+        action = text[0]
+        args = text[1:]
+        if action == "turn_head":
+    
+    match = re.search(r'\[FORGET: (.*?)\]', response)
+    if match:
+        text = match.group(1).split()
+        action = text[0]
+        args = text[1:]
+        if action == "turn_head":
+                
             ## TODO: ADD IFs TO RUN FUNCTIONS
     server.send_to_client(edited_response)
