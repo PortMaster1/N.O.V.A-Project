0ce7c3d8bb9f629b32aae9b1dbf090a035aabc6f
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-11 17:17:13 -0400

    Update main.py

--- Onboard-Setup/main.py
+++ Onboard-Setup/main.py
@@ -1,28 +1,30 @@
 from llm_system import get_response
-import server
+from sensors import listen, speak
 import re
 from memory_lane import remember, forget
 
 
-
-
-
 def main_loop():
-    user_input = server.listen()
+    user_input = listen()
     response = get_response(user_input)
+    match = re.search(r'\[ACTION: (.*?)\]', response)
+    if match:
+        text = match.group(1).split()
+        action = text[0]
+        args = text[1:]
+        if action == "turn_head":
+            turn_head(*args)
+        response.replace(r'\[ACTION: (.*?)\]', '')
     match = re.search(r'\[REMEMBER: (.*?)\]', response)
     if match:
         text = match.group(1).split()
-        action = text[0]
-        args = text[1:]
-        if action == "turn_head":
-    
+        menory = text[0:]
+        remember(memory)
+        response.replace(r'\[REMEMBER: (.*?)\]', '')
     match = re.search(r'\[FORGET: (.*?)\]', response)
     if match:
         text = match.group(1).split()
-        action = text[0]
-        args = text[1:]
-        if action == "turn_head":
-                
-            ## TODO: ADD IFs TO RUN FUNCTIONS
-    server.send_to_client(edited_response)
+        memory = text[0:]
+        forget(memory)
+        response.replace(r'\[FORGET: (.*?)\]', '')
+    speak(respomse)
