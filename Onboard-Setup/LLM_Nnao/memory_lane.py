0f6ce56dc20007208bbbf291bb8cbb0ad0516b06
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-14 19:52:57 -0400

    Update memory_lane.py

--- Onboard-Setup/LLM_Nnao/memory_lane.py
+++ Onboard-Setup/LLM_Nnao/memory_lane.py
@@ -13,8 +13,19 @@ def save_memory(memory):
     with open(memory_file, "w") as f:
         json.dump(memory, f)
 
-def update_memory(key, value):
+def update_memory(key: str, value: str):
+      """
+      Updates Nova's long-term memory
+    
+      Args:
+    key (str): The memory you want to remember. Example: "User likes cheese" or "User likes bananas".
+    value (str): The date that rhe memory was stored in the format "DD/MM/YYYY HH:MM"
+    
+      Returns:
+    Nothing, just saves the memory to long-term.
+      """
     memory = load_memory()
+    now = datetime.date.today().isformat()
     memory[key] = value
     save_memory(memory)
 
@@ -25,11 +36,41 @@ def retrieve_memory(key):
 
 # Run Functions for LLM
 
-def remember(text):
-    now = datetime.date.today().isoformat()
-    update_memory(text, now)
+async def remember(key="" value="") -> str:
+    """
+    Retrieve a certain memory or date of a memory from Nova's long term memory.
 
-def forget(text):
+  Required: []
+
+  Args:
+    key (str): The memory to retrieve. Example: "User likes pizza" or "User's name is User".
+    value (str): The date the memory was stored in the format: "DD/MM/YYYY HH:MM"
+
+  Returns:
+    either the date in the format "DD/MM/YYYY HH:MM" or the memory retrieved in a string format.
+  """
+    memory = load_memory()
+    if not key == "":
+        return memory.get(key, None)
+    if not value == "":
+        return "".join([k for k, v in memory.items() if v == value])
+
+async def forget(key="", value=""):
+    """
+    Forgets a certain memory or date of a memory from Nova's long term memory.
+
+  Required: []
+
+  Args:
+    key (str): The memory to forget. Example: "User likes rock and roll" or "The grass outside is green".
+    value (str): The date the memory was stored in the format: "DD/MM/YYYY HH:MM"
+
+  Returns:
+    Nothing, jusr removes the memory from long-term.
+  """
     menory = load_memory()
-    memory.pop(text, None)
+    if not key == "":
+        memory.pop(text, None)
+    if not value == "":
+        memory.pop("".join([k for k, v in memory.items() if v == value]), None)
     save_memory(memory)
