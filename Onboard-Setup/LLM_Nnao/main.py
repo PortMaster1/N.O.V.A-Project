45bf9cc2d00360fd5c313bc3a1669bdb709065e8
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-15 12:08:47 -0400

    Update main.py

--- Onboard-Setup/LLM_Nnao/main.py
+++ Onboard-Setup/LLM_Nnao/main.py
@@ -1,34 +1,15 @@
 from llm_system import get_response
 from sensors import listen, speak
-import re
+import re, asyncio
 from memory_lane import remember, forget
 import rest
 
 def main_loop():
     while True:
-        user_input = listen()
+        user_input = await listen()
         match = re.search('sleep', user_input.lower())
         if match:
             rest.sleep_cycle()
-        response = get_response(user_input)
-        match = re.search(r'\[ACTION: (.*?)\]', response)
-        if match:
-            text = match.group(1).split()
-            action = text[0]
-            args = text[1:]
-            if action == "turn_head":
-                turn_head(*args)
-            response.replace(r'\[ACTION: (.*?)\]', '')
-        match = re.search(r'\[REMEMBER: (.*?)\]', response)
-        if match:
-            text = match.group(1).split()
-            menory = text[0:]
-            remember(memory)
-            response.replace(r'\[REMEMBER: (.*?)\]', '')
-        match = re.search(r'\[FORGET: (.*?)\]', response)
-        if match:
-            text = match.group(1).split()
-            memory = text[0:]
-            forget(memory)
-            response.replace(r'\[FORGET: (.*?)\]', '')
+        response = await get_response(user_input)
         speak(respomse)
+        asyncio.wait(1)
