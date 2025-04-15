e0dbc9684514f7745bf74fa4dcc50dac3408aa66
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-15 12:16:59 -0400

    Update main.py

--- Onboard-Setup/LLM_Nnao/main.py
+++ Onboard-Setup/LLM_Nnao/main.py
@@ -1,15 +1,16 @@
 from llm_system import get_response
 from sensors import listen, speak
-import re, asyncio
+import re
+from time import sleep
 from memory_lane import remember, forget
 import rest
 
 def main_loop():
     while True:
-        user_input = await listen()
+        user_input = listen()
         match = re.search('sleep', user_input.lower())
         if match:
             rest.sleep_cycle()
-        response = await get_response(user_input)
+        response = get_response(user_input)
         speak(respomse)
-        asyncio.wait(1)
+        sleep(0.1)
