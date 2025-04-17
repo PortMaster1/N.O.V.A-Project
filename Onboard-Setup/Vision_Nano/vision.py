fccf59deff0b95e3ed4196072f59595ed97b632e
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-17 18:12:17 -0400

    Update vision.py

--- Onboard-Setup/Vision_Nano/vision.py
+++ Onboard-Setup/Vision_Nano/vision.py
@@ -8,7 +8,7 @@ import cv2
 import numpy as np
 import insightface
 from insightface.app import FaceAnalysis
-from insightface.data import get_image as ins_get_image
+from insightface.data import get_image, get_object
 
 
 
