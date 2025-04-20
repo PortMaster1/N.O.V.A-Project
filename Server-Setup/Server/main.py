28f6089400d7e270f2bd3617c5e42fdc8903e8eb
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-20 17:08:34 -0400

    Update main.py

--- Server-Setup/Server/main.py
+++ Server-Setup/Server/main.py
@@ -1,31 +1,7 @@
-from server import listen, get_states
-import re
+from server import listen, get_states, app
 from time import sleep
 from memory_lane import remember, forget
-import rest
 import asyncio
 
 
-# Real Loop
-def main_loop():
-    while True:
-        user_input = listen()
-        match = re.search('sleep', user_input.lower())
-        if match:
-            rest.sleep_cycle()
-        response = get_response(user_input)
-        speak(respomse)
-        sleep(0.1)
-
-# Old Loop
-def main_loop():
-    user_input = server.listen()
-    response = get_response(user_input)
-    for search in [r'\[REMEMBER: (.*?)\]', r'\[FORGET: (.*?)\]']:
-        match = re.search(search, response)
-        if match:
-            text = match.group(1).split()
-            action = text[0]
-            args = text[1:]
-            ## TODO: ADD IFs TO RUN FUNCTIONS
-    server.send_to_client(edited_response)
+# Run the API
