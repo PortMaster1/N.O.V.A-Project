f09939e99bdfdfc8aa6a674cc5c0c83866afb56f
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-20 16:59:34 -0400

    Update server.py

--- Server-Setup/Server/server.py
+++ Server-Setup/Server/server.py
@@ -1,23 +1,16 @@
 import asyncio, requests
 from fastapi import FastAPI
 from llm_system import get_response
+import re
 
 app = FastAPI()
 
-old_text = ""
-text = ""
 states = {
     "x": 0.0,
     "y": 0.0,
     "z": 0.0,
     "tilt": 0.0
 }
-
-def _get_old_text():
-    return old_text
-
-def _get_text():
-    return text
 
 def get_states():
     return states
@@ -36,23 +29,8 @@ async def receive_states(state=[]):
     states.tilt = tilt
     return {"message": "States Received"}
 
-@app.post("/listen/{text}")
-async def listen_api(user_input=""):
-    global text
-    text = user_input
-    return {"message": "Text Received"}
-
 @app.post("/chat/{message}")
 async def chat(message):
+    match = re.search(message.lower(), "nova, sleep")
     response = get_resposnse(message)
     return response
-
-async def wait_until_unequal(func1, func2, check_interval=0.1):
-    while func1() == func2():
-        await asyncio.sleep(check_intetval)
-
-async def listen():
-    if _get_old_text() == _get_text():
-        await wait_until_unequal(_get_old_text, _get_text)
-    old_text = _get_text()
-    return _get_text()
