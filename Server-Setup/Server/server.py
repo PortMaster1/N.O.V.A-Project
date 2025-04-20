468e10b3ee64e7a9e9c83cffe6dbe86430bb7e3c
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-20 10:51:27 -0400

    Create server.py

---
+++ Server-Setup/Server/server.py
@@ -0,0 +1,12 @@
+import asyncio, requests
+
+async def listen():
+    text = ""
+    return text
+
+async def speak(text):
+    pass
+
+async def get_states():
+    tilt = x = y = z = None
+    return [tilt, x, y, z]
