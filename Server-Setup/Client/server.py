56609894dd83fd1e9a4108129945515e1b90aadc
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-20 16:12:40 -0400

    Update server.py

--- Server-Setup/Client/server.py
+++ Server-Setup/Client/server.py
@@ -1,8 +1,16 @@
 import asyncio, requests
 
 # REST API URL of your LLM server
-CHAT_URL = "http://192.168.12.141:8000/chat"  # Update IP
+IP = "192.168.12.141:8000"            # Update IP Address
+CHAT_URL = f"http://{IP}/chat"
+STATE_URL = f"http://{IP}/state"
 
 async def get_response(text, model):
     response = await requests.post(CHAT_URL, json={"message": text})
     return response.json()["response"]
+
+async def send_states(x, y, z, tilt):
+    requests.post(STATE_URL, json={"states": [x, y, z, tilt]})
+    return
+
+async def 
