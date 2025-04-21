0553785b733a0f561e7607bed5b425b28c6ec313
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-21 11:00:31 -0400

    Update main.py

--- Server-Setup/Client/main.py
+++ Server-Setup/Client/main.py
@@ -1,6 +1,11 @@
 import asyncio
-from server import 
+from server import send_states, get_response
+from sensors import get_states
+
+async def send_state_loop():
+    while True:
+        await send_states()
 
 def main_loop():
     while True:
-        asyncio.run(
+        asyncio.run()
