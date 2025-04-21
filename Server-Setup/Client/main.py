d50efe0ae4af489bdd89f92f83c6635f9f26e3e9
Dalton Knapp <knapptime_wv@outlook.com>
2025-04-21 11:12:45 -0400

    Update main.py

--- Server-Setup/Client/main.py
+++ Server-Setup/Client/main.py
@@ -4,9 +4,14 @@ from sensors import read_microbit, listen, speak
 
 async def send_state_loop():
     while True:
-        x, y, z, tilt = read_microbit()
+        x, y, z, tilt = await read_microbit()
         await send_states(x, y, z, tilt)
+        await asyncio.sleep(0.1)
+
+async def llm_loop():
+    while True:
+        
 
 def main_loop():
-    while True:
-        asyncio.run()
+    asyncio.run(send_state_loop)
+    asyncio.run(llm_loop)
