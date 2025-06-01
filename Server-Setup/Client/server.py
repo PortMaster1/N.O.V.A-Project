7b73f61828c726b6cc79d3f6b0d7374fe3a01879
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-01 11:31:22 -0400

    Update server.py

--- Server-Setup/Client/server.py
+++ Server-Setup/Client/server.py
@@ -1,6 +1,6 @@
 import asyncio, requests
 # Testing Imports
-import asyncio, socket
+import asyncio, socket, signals
 
 # REST API URL of your LLM server
 IP = "192.168.12.138:8000"            # Update IP Address
@@ -17,8 +17,21 @@ async def send_states(x, y, z, tilt):
 
 # Testing Starts Here
 
+signals = signals.Signals()
+
 class Client:
     def __init__(self, HOST, PORT):
         self._HOST = HOST
         self._PORT = PORT
-        
+        # Initialize Socket Server
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    async def start_listening(self):
+        self.s.connect(self._HOST, self._PORT)
+        while True:
+            data = self.s.recv(1024).decode()
+            print("Received: " + data)
+    
+    async def send(self, data):
+        encoded = data.encode()
+        self.s.sendall(encoded)
