f37e13e42f7c144f4431038989f71da73fb3c1b2
Dalton Knapp <knapptime_wv@outlook.com>
2025-08-03 10:25:52 -0400

    Update server.py

--- Server-Setup/Server/server.py
+++ Server-Setup/Server/server.py
@@ -2,7 +2,9 @@ import asyncio, requests, re
 from flask import Flask, request, jsonify, render_template
 from llm_system import *
 # Testing Imports
-import asyncio, socket, json
+import asyncio
+import websockets
+import json
 
 # Set up functions because Python is weird
 """
@@ -12,80 +14,74 @@ get_chat_mem is from llm_system
 
 app = Flask(__name__)
 
-states = {
-    "x": 0.0,
-    "y": 0.0,
-    "z": 0.0,
-    "tilt": 0.0
-}
-
 def get_states():
-    return states
-
+	return states
+	
 @app.route("/")
 async def root():
-    return {"message": "Hello World"}
-
-@app.route("/api/state", methods=["POST"])
-async def receive_states():
-    state = request.get_json()['state']
-    global states
-    x, y, z, tilt = state.split()
-    statss.x = x
-    states.y = y
-    states.z = z
-    states.tilt = tilt
-    return {"message": "States Received"}
-
+	return {"message": "Hello World"}
+	
 @app.route("/api/chat", methods=["POST"])
 async def apichat():
-    message = request.get_json()['message']
-    print(message)
-    match = re.search("nova, sleep", message.lower())
-    if match:
-        # await rest.sleep_cycle()
-        pass # TODO: FIX LATER
-    else:
-        response = await get_response(message).content
-    return {"response": response}
-
+	message = request.get_json()['message']
+	print(message)
+	match = re.search("nova, sleep", message.lower())
+	if match:
+		# await rest.sleep_cycle()
+		pass # TODO: FIX LATER
+	else:
+		response = await get_response(message).content
+	return {"response": response}
+	
 @app.route("/chat", methods=["GET","POST"])
 async def chat():
-    if request.method == "POST":
-        message = request.get_form()['message']
-        print(message)
-        #response = get_response(message)
-        response = "This is an example response and needs to be replaced in N.O.V.A-Project/Server-Setup/Server/server.py."
-    chat_mem = get_chat_mem()
-    return render_template("index.html", chat_mem=chat_mem)
-
+	if request.method == "POST":
+		message = request.get_form()['message']
+		print(message)
+		#response = get_response(message)
+		response = "This is an example response and needs to be replaced in N.O.V.A-Project/Server-Setup/Server/server.py."
+	chat_mem = get_chat_mem()
+	return render_template("index.html", chat_mem=chat_mem)
+	
 # Start Testing Here:
 
 HOST = "0.0.0.0"
-PORT = 8080
+PORT = 1052
+URL = f"ws://{HOST}:{PORT}"
 
-URL = f"{HOST}:{PORT}"
 
 class Server:
-    def __init__(self, HOST, PORT):
-        self._HOST = HOST
-        self._PORT = PORT
-        self.signals = signals
-        # Start Socket Server
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bimd(HOST, PORT)
-    
-    async def start_listening(self):
-        self.s.listen()
-        print(f"Server Listening on {self._HOST}:{self._PORT}")
-        conn, addr = self.s.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            while True:
-                self.data = conn.recv(1024)
-                print("RECEIVED: ", data.decode())
-    
-    async def send(self, data):
-        encoded = data.encode()
-        self.s.sendall(encoded)
-        print(f"SENT DATA: {data}")
+	def __init__(self, host, port):
+		self._HOST = host
+		self._PORT = port
+		self.clients = set()
+	
+	
+	async def handler(self, websocket, path):
+		print("Client Connected")
+		self.clients.add(websocket)
+		try:
+			async for message in websocket:
+				message = json.dumps(message, indent=2)
+				print("Received: " + message)
+		except websockets.ConnectionClosed:
+			print("Client Disconnected")
+		finally:
+			self.clients.remove(websocket)
+	
+	
+	async def send(self, data):
+		data = json.loads(data)
+		for client in self.clients:
+			await client.send(data)
+		print(f"Sent data to client(s)")
+	
+	
+	async def start(self):
+		print(f"Starting Websocket on ws://{self._HOST}:{self._PORT}")
+		async with websockets.serve(self.handler, self._HOST, self._PORT):
+			await asyncio.Future() # Run Forever
+
+if __name__ == "__main__":
+	server = Server()
+	asyncio.run(server.start())
