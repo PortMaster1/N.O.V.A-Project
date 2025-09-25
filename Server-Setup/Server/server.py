c80697bd359207a2156d8380bf129edfc9b3a083
Dalton Knapp <knapptime_wv@outlook.com>
2025-09-25 19:23:19 -0400

    Update server.py

--- Server-Setup/Server/server.py
+++ Server-Setup/Server/server.py
@@ -1,87 +1,69 @@
 import asyncio, requests, re
-from flask import Flask, request, jsonify, render_template
-from llm_system import *
-# Testing Imports
 import asyncio
-import websockets
 import json
+import discord
 
-# Set up functions because Python is weird
-"""
-get_response is from llm_system
-get_chat_mem is from llm_system
-"""
+intents = discord.Intents.default()
+intents.message_content = True
 
-app = Flask(__name__)
+class Discord_Client:
+	def __init__(self, signals):
+		self.signals = signals
+	
+	# Runs the client alltogether
+	async def run(self):
+		global signals
+		signals = self.signals
+		
+		bot = discord.Bot()
+		connections = {}
+		
+		@bot.event
+		async def on_ready():
+			print("DISCORD BOT IS READY")
+			pm1 = await bot.fetch_user("USER_ID")
+			await pm1.send("Online")
+			self.channel = await pm1.create_dm()
+		
+		@bot.event
+		async def on_message(message):
+			if message.author == bot.user:
+				return
+			if message.channel not in connections:
+				connections.append(message.channel)
+			# Add the message to the history and mark a new message.
+			self.signals.history.append({"role":"user", "content":message.content})
+			self.signals.new_message = True
+		
+		@bot.slash_command(name="ping", description="Check the bot's status")
+		async def ping(ctx):
+			await ctx.respond(f"Pong! {bot.latency}")
+		
+		# Starts the "is typing..." text on Discord
+		async def start_typing():
+			while not self.signals.terminate:
+				if self.signals.AI_thinking:
+					await self.channel.typing()
+					await asyncio.sleep(10)
+		
+		# Send the initial message for the response
+		async def send_initial(message):
+			self.signals.AI_speaking = True
+			self.message = await self.channel.send(message)
+		
+		# Edit the previously created message to include the additional chunk
+		async def send_message(message):
+			await self.message.edit(message)
+		
+		# Create the start_typing task and start the bot
+		asyncio.create_task(start_typing())
+		bot.run("TOKEN", status="online")
 
-def get_states():
-	return states
-	
-@app.route("/")
-async def root():
-	return {"message": "Hello World"}
-	
-@app.route("/api/chat", methods=["POST"])
-async def apichat():
-	message = request.get_json()['message']
-	print(message)
-	match = re.search("nova, sleep", message.lower())
-	if match:
-		# await rest.sleep_cycle()
-		pass # TODO: FIX LATER
-	else:
-		response = await get_response(message).content
-	return {"response": response}
-	
-@app.route("/chat", methods=["GET","POST"])
-async def chat():
-	if request.method == "POST":
-		message = request.get_form()['message']
-		print(message)
-		#response = get_response(message)
-		response = "This is an example response and needs to be replaced in N.O.V.A-Project/Server-Setup/Server/server.py."
-	chat_mem = get_chat_mem()
-	return render_template("index.html", chat_mem=chat_mem)
-	
-# Start Testing Here:
-
-HOST = "0.0.0.0"
-PORT = 1052
-URL = f"ws://{HOST}:{PORT}"
-
-
-class Server:
-	def __init__(self, host, port):
-		self._HOST = host
-		self._PORT = port
-		self.clients = set()
-	
-	
-	async def handler(self, websocket, path):
-		print("Client Connected")
-		self.clients.add(websocket)
-		try:
-			async for message in websocket:
-				message = json.dumps(message, indent=2)
-				print("Received: " + message)
-		except websockets.ConnectionClosed:
-			print("Client Disconnected")
-		finally:
-			self.clients.remove(websocket)
-	
-	
-	async def send(self, data):
-		data = json.loads(data)
-		for client in self.clients:
-			await client.send(data)
-		print(f"Sent data to client(s)")
-	
-	
-	async def start(self):
-		print(f"Starting Websocket on ws://{self._HOST}:{self._PORT}")
-		async with websockets.serve(self.handler, self._HOST, self._PORT):
-			await asyncio.Future() # Run Forever
 
 if __name__ == "__main__":
-	server = Server()
-	asyncio.run(server.start())
+	from signals import Signals
+	import threading
+	signals = Signals()
+	client = Discord_Client(signals)
+	client_thread = threading.Thread(target=client.run, dameon=True)
+	client_thread.start()
