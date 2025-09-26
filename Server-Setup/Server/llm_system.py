39ae0f0d4548c30b279f90bbbe8f919275389db4
Dalton Knapp <knapptime_wv@outlook.com>
2025-09-26 15:46:27 -0400

    Update llm_system.py

--- Server-Setup/Server/llm_system.py
+++ Server-Setup/Server/llm_system.py
@@ -1,103 +1,75 @@
-from mem0 import Memory
-import emotion_core
-from ollama import chat, ChatResponse
+# Python Imports
 import asyncio
 import time
+import configparser
 
-# Stopwatch
-start = None
+# Local Imports
+from ollama import ChatResponse, AsyncClient
 
-# Persistsnt mem0 Memory
-config = {
-    "vector_store": {
-        "provider": "qdrant",
-        "config": {
-            "host": "localhost",
-            "port": 6333,
-            "embedding_model_dims": 1024,
-            "on_disk": True,
-        }
-    },
-    "llm": {
-        "provider": "ollama",
-        "config": {
-            "model": "llama3.2:1B"
-        }
-    },
-    "embedder": {
-        "provider": "ollama",
-        "config": {
-            "model": "mxbai-embed-large"
-        }
-    },
-}
+# for the LLM model
+config = configparser.ConfigParser()
+config.read('config.ini')
+config = config['DEFAULT']
 
-# Initialize Memory
-memory = Memory.from_config(config)
-
-# Temporary Conversation Memory
-chat_mem = []
-
-# Functions callable by LLM
-available_functions = {}
+class LLM_System:
+	def __init__(self, signals):
+		self.signals = signals
+		# Functions callable by LLM
+		self.aval_functions = {}
+		
+	async def get_response(self, model: str = None, user_id: str = "default_user"):
+		if not model:
+			model = config["model"]
+		# TODO: Do this: v
+		#response = chat(model, messages=chat_mem, tools=[update_memory, update_emotions, remember, forget]
+		async for chunk in await AsyncClient().chat(model=model,messages=self.signals.history, streaming=True):
+			if not self.signals.AI_speaking:
+				self.signals.history[{"role":"assistant","content":chunk['message']['content']}]
+			else:
+				self.signals.history[-1]["content"] += chunk['message']['content']
+		
+	def run(self):
+		print("LLM IS RUNNING")
+		while not self.signals.terminate:
+			if self.signals.new_message:
+				self.signals.new_message = False
+				await self.get_response()
 
 
-def get_chat_mem() -> list:
-    global chat_mem
-    return chat_mem
-
-# Get the response from the LLM
-async def get_response(message: str, model: str = "nova4.1", user_id: str = "default_user") -> str:
-    global chat_mem
-    global start
-    if not start:
-        start = time.perf_counter()
-    end = time.perf_counter()
-    if end - start > 60:
-        if not chat_mem == []:
-            memory.add(chat_mem, user_id=user_id)
-            chat_mem = []
-    
-    relevant_memories = memory.search(query=message, user_id=user_id)
-    if relevant_memories:
-        prompt = f"User input: {message}\nRelevant memories: {'\n'.join(relevant_memories)}"
-    else:
-        prompt = f"User input: {message}"
-    
-    memories_str = "\n".join(f"- {entry['memory']}" for entry in relevant_memories["results"])
-    chat_mem.append({"role": "user", "content": prompt})
-    #response = chat(model, messages=chat_mem, tools=[update_memory, update_emotions, remember, forget]
-    response = chat(model, messages=chat_mem)
-    print(response.message.content)
-    chat_mem.append({"role": "assistant", "content": response.message.content})
-    
-    if response.message.tool_calls:
-      # There may be multiple tool calls in the response
-      for tool in response.message.tool_calls:
-        # Ensure the function is available, and then call it
-        if function_to_call := available_functions.get(tool.function.name):
-          print('Calling function:', tool.function.name)
-          print('Arguments:', tool.function.arguments)
-          output = function_to_call(**tool.function.arguments)
-          print('Function output:', output)
-          # Add the function response to messages for the model to use
-          chat_mem.append(response.message)
-          chat_mem.append({'role': 'tool', 'content': str(output), 'name': tool.function.name})
-        else:
-          print('Function', tool.function.name, 'not found')
-          bringupanerrorfromthistextbecauseiforgothowtoraiseonepropely
-        response = chat(model, messages=memories_str)
-        print(response.message.content)
-        chat_mem.append ({"role": "assistant", "content": response.message.content})
-    start = time.perf_counter()
-    return chat_mem
-
-
+# TODO: Implement tool stuff v
+"""
+if response.message.tool_calls:
+	# There may be multiple tool calls in the response
+	for tool in response.message.tool_calls:
+	# Ensure the function is available, and then call it
+		if function_to_call := available_functions.get(tool.function.name):
+			print('Calling function:', tool.function.name)
+			print('Arguments:', tool.function.arguments)
+			output = function_to_call(**tool.function.arguments)
+			print('Function output:', output)
+			# Add the function response to messages for the model to use
+			chat_mem.append(response.message)
+			chat_mem.append({'role': 'tool', 'content': str(output), 'name': tool.function.name})
+		else:
+			print('Function', tool.function.name, 'not found')
+			bringupanerrorfromthistextbecauseiforgothowtoraiseonepropely
+		response = chat(model, messages=memories_str)
+		print(response.message.content)
+		chat_mem.append ({"role": "assistant", "content": response.message.content})
+"""
+	
 if __name__ == '__main__':
-    try:
-        while True:
-            message = input(" You say:")
-            #asyncio.run(get_response(input_text))
-            get_response(message)
-    except KeyboardInterrupt:
-        print("Goodbye!")
+	from signals import Signals
+	import threading
+	signals = Signals()
+	llm = LLM_System(signals)
+	llm_thread = threading.Thread(target=llm.run, dameon=True)
+	try:
+		while True:
+			message = input(" You say:")
+			message = {"role":"user","content":message}
+			signals.history.append(message)
+			if self.signals.history[-1] != message:
+				print(self.signals.history[-1]["content"])
+	except KeyboardInterrupt:
+		print("Goodbye!")
